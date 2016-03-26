3ae3af98f3118fe26902bbdb9e56d11daa5d8301
ccasal <casal.christian@gmail.com>
2016-03-26 18:26:26 -0400

    trying google sign in auth

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
+$("#loginModal").on('show.bs.modal', function(event){
+  var button = $(event.logInButton) // Button that triggered the modal
+  var recipient = button.data('login');
+    $('.modal-title').text('login');
+  var createGoogleAuth = $('<div class="g-signin2" data-onsuccess="onSignIn"></div>')
+    $('.modal-body').append(createGoogleAuth);
+});
 
 $("#articleModal").on('show.bs.modal', function(event){
   var button = $(event.logInButton) // Button that triggered the modal
