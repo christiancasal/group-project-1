e50b928c723c1f38e66f447b261d3c8f70d430e2
ccasal <casal.christian@gmail.com>
2016-03-26 18:10:35 -0400

    trying google sign in auth

--- js/app.js
+++ js/app.js
@@ -1,34 +1,6 @@
-// $("#loginModal").on('show.bs.modal', function(event){
-//   var button = $(event.logInButton) // Button that triggered the modal
-//   var recipient = button.data('login');
-//   var fbButton = createFBLoginHTML();
-//     $('.modal-title').text('login');
-//     $('.modal-body').append(fbButton);
-//
-// FB.getLoginStatus(function(response) {
-//   if (response.status === 'connected') {
-//     console.log('Logged in.');
-//     debugger;
-//   }
-//   else {
-//     debugger;
-//     FB.login();
-//   }
-// });
-// FB.login(function(response) {
-//   // handle the response
-//   console.log(response);
-// }, {scope: 'public_profile,email'});
-//
-// });
 
 $("#articleModal").on('show.bs.modal', function(event){
   var button = $(event.logInButton) // Button that triggered the modal
   var recipient = button.data('login');
   $('.modal-title').text('write your story');
 });
-
-function createFBLoginHTML(){
-  var fbButton = $('<fb: login-button scope="public_profile,email" onlogin="checkLoginState()"</fb:login-button><div id="status"></div>')
-  return fbButton;
-}
