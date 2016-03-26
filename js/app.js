d9bc0f48de6049be765b18fdbbcfdf6855c6927d
ccasal <casal.christian@gmail.com>
2016-03-26 11:18:30 -0400

    login button test

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ $("#loginModal").on('show.bs.modal', function(event){
   var button = $(event.logInButton) // Button that triggered the modal
   var recipient = button.data('login');
   $('.modal-title').text('login');
-  //createFBLoginHTML();
+  createFBLoginHTML();
 });
 
 $("#articleModal").on('show.bs.modal', function(event){
@@ -11,7 +11,7 @@ $("#articleModal").on('show.bs.modal', function(event){
   $('.modal-title').text('write your story');
 });
 
-// function createFBLoginHTML(){
-//   var fbButton = $('<fb: login-button scope="public_profile,email" onlogin="checkLoginState()"; </fb:login-button><div id="status"></div>')
-//
-// }
+function createFBLoginHTML(){
+  var fbButton = $('<fb: login-button scope="public_profile,email" onlogin="checkLoginState()"; </fb:login-button><div id="status"></div>')
+
+}
