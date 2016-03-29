b2297c0189d47bc5820bceff36bdb87386ee8e98
ccasal <casal.christian@gmail.com>
2016-03-28 22:46:55 -0400

    trying fb signin  auth

--- js/app.js
+++ js/app.js
@@ -1,8 +1,7 @@
-// $(function(){
-//
-// });
-$('#logInButton').on('click', function(){
-  console.log('hello');
+function checkFB(){
+debugger;
+  var hello = "hello";
+  console.log(hello);
   var ref = new Firebase("https://google-login-read-people.firebaseio.com");
   ref.authWithOAuthPopup("facebook", function(error, authData) {
     if (error) {
@@ -11,10 +10,9 @@ $('#logInButton').on('click', function(){
       console.log("Authenticated successfully with payload:", authData);
     }
   });
-});
-
+}
 $("#articleModal").on('show.bs.modal', function(event){
-  var button = $(event.logInButton) // Button that triggered the modal
+  var button = $(event.articleModal) // Button that triggered the modal
   var recipient = button.data('login');
   $('.modal-title').text('write your story');
 });
